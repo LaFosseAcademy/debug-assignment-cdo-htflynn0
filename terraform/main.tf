986922733ddc989115e2d30f1a9df8fb416a1e13
Harrison <htflynn0@gmail.com>
2025-02-20 09:45:00 +0000

    Update main.tf with instance type and ami

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,9 +12,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "http_server" {
-  ami = ""
+  ami = "ami-053a45fff0a704a47"
   key_name = "default-ec2"
-  instance_type = ""
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.http_server_sg.id]
   subnet_id = data.aws_subnets.default_subnets.ids[0]
   connection {
